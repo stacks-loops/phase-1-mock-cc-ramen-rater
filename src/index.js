e376b5bbe6d921cc5b46baf308a41b06684111aa
Parker <p.cronin.connolly@gmail.com>
2024-01-18 10:50:31 -0500

    fetch written

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // write your code here 
+fetch('http://localhost:3000/ramens')
+.then(resp => resp.json())
+.then(data => console.log(data))
+
+// fetch the data 
+// create js variables
+// query select or create where they will go in HTML(DOM)
+// Attach event listeners
+// Assign data to the DOM elements
+// Append the new eleemnts to the DOM
